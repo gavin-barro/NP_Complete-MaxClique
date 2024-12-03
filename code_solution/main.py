e487584fcfe5ffd956972c2e31d1e04368f0bc8c
Cole <determsc@dukes.jmu.edu>
2024-12-03 10:24:40 -0500

    Hi

--- code_solution/main.py
+++ code_solution/main.py
@@ -2,10 +2,19 @@
 graph = {'A': ['B', 'C'],
          'B': ['A', 'C'],
          'C': ['A', 'B']
-        }
+         }
+
+
+def solver(graph G):
+    biggest_clique = []
+    for v in G:
+        for e in v:
+            if e in biggest_clique:
+
 
 def main() -> None:
     pass
+
 
 if __name__ == "__main__":
     main()
